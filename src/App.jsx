cc5bf061cae59b2e16d0f8ffee378cfabdd30231
shivam <shivamsinghmaurya9@gmail.com>
2025-10-16 13:10:18 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import Content from './components/content'
+
 import PortfolioApp from './shivam_professional_web_developer_portfolio_react_tailwind'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
